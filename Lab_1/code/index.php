944a177c5b04ba4732bda5bd66c313c064425dce
kolgyy <eduardkorotaev77@gmail.com>
2024-03-16 02:56:33 +0200

    Updated to Lab_2_Task_1

--- Lab_1/code/index.php
+++ Lab_1/code/index.php
@@ -1,2 +1,17 @@
 <?php
-echo phpinfo();
+// ------ Task 1 ------
+
+/* Imagine a lot of code here */
+$very_bad_unclear_name = "15 chicken wings";
+
+// Write your code here:
+
+$order =& $very_bad_unclear_name; // Указали переменной ссылку на другую переменную
+$order .= ' & 4 hamburgers'; // Произвели конкатенацию строк
+
+// Don't change the line below
+echo "\nYour order is: $very_bad_unclear_name.\n"; // Будет выведена строка "15 chicken wings & 4 hamburgers" ,т.к. переменные указывают на одну и ту же область в памяти
+
+// ------ Task 2 ------
+
+
